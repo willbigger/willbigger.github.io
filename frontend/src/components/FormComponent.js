0af51d88b8ab607f1ee0990ff901df4d3e75d81f
willbigger <wmb8yt@virginia.edu>
2022-02-28 20:08:46 -0500

    Footer and the rest of cosmetic branch

--- frontend/src/components/FormComponent.js
+++ frontend/src/components/FormComponent.js
@@ -163,6 +163,7 @@ stored as state variables.
       if (inputs.infectionSite.includes(event.target.value)) {
         let temp = inputs.infectionSite.indexOf(event.target.value)
         delete inputs.infectionSite[temp]
+        inputs.infectionSite.length -= 1;
       }
     }
     console.log(inputs.infectionSite)
@@ -296,6 +297,12 @@ stored as state variables.
     setPathogenToggle(false);
     setnecToggle(false);
     document.getElementById("input-form").reset();
+    for (let i = 0; i < inputs.infectionSite.length; i++) {
+      delete inputs.infectionSite[i];
+    }
+    inputs.infectionSite.length = 0;
+    document.querySelectorAll('input[type="checkbox"]')
+    .forEach(el => el.checked = false);
 
     setInputs({
       ...inputs,
