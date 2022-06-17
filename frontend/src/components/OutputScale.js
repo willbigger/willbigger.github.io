f574a2ab80bcb453f3eadf023d224758a3ad6fbd
calvinkuo <14984741+calvinkuo@users.noreply.github.com>
2022-06-17 10:08:30 -0700

    add separate labels to OutputScale

--- frontend/src/components/OutputScale.js
+++ frontend/src/components/OutputScale.js
@@ -11,10 +11,14 @@ function OutputScale({ activeIndex, onSelect }) {
 
   return (
     <div className="output-scale">
-      <Button className="choice-1" active={activeIndex === 0} onClick={onClick(0)}>Broadest</Button>
-      <Button className="choice-2" active={activeIndex === 1} onClick={onClick(1)}>&nbsp;</Button>
-      <Button className="choice-3" active={activeIndex === 2} onClick={onClick(2)}>&nbsp;</Button>
-      <Button className="choice-4" active={activeIndex === 3} onClick={onClick(3)}>Narrowest</Button>
+      <div className="choice-label">Narrowest</div>
+      <Button className="choice-1" active={activeIndex === 0} onClick={onClick(0)}>1st Choice</Button>
+      <div className="choice-label"><span className="arrow"></span></div>
+      <Button className="choice-2" active={activeIndex === 1} onClick={onClick(1)}>2nd Choice</Button>
+      <div className="choice-label"><span className="arrow"></span></div>
+      <Button className="choice-3" active={activeIndex === 2} onClick={onClick(2)}>3rd Choice</Button>
+      <div className="choice-label">Broadest</div>
+      <Button className="choice-4" active={activeIndex === 3} onClick={onClick(3)}>4th Choice</Button>
     </div>
   )
 }
