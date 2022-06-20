6f861efc802f7de9d34729695964bdbf3a243af5
jts-hong <jhong1711785634@gmail.com>
2022-06-20 17:26:50 -0400

    img fetch

--- frontend/src/components/TreatmentInfo.js
+++ frontend/src/components/TreatmentInfo.js
@@ -1,9 +1,25 @@
 import React from 'react';
-import aaa from './aaa.png';
+import aaa from './DosingImg/Acyclovir.png';
 
 function TreatmentInfo({ treatment }) {
+    var treat = treatment.toString();
+    var test = './DosingImg/Ampicillin.png'
+    var dosingName = './DosingImg/' +treat+'.png'
     return (
-        <img className="treatment-info" src={aaa} alt={treatment}></img>
+        <> 
+           
+            <p> {dosingName}</p>
+            {/*
+            <img className="treatment-info" src={aaa} alt={treatment}></img>
+            <img className="treatment-info" src={require(str)} alt={treatment}></img>  */}
+           
+            {/*
+
+            <img className="treatment-info" src={require(`${dosingName}`)}alt={treatment}></img>
+
+            */}
+            <img className="treatment-info" src={require(`${test}`)}alt={treatment}></img>
+        </>
     );
 }
 
