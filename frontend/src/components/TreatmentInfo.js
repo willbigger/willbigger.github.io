2c7edaeaf52a1e4bf40e68f03568fd902316d698
jts-hong <jhong1711785634@gmail.com>
2022-06-22 12:18:54 -0400

    new

--- frontend/src/components/TreatmentInfo.js
+++ frontend/src/components/TreatmentInfo.js
@@ -3,29 +3,10 @@ import aaa from './DosingImg/Acyclovir.png';
 import ReactImageFallback from "react-image-fallback";
 
 function TreatmentInfo({ treatment }) {
-    var treat = treatment.toString();
-    var test = './DosingImg/Ampicillin.png'
-    var dosingName = './DosingImg/' +treat+'.png'
+
     return (
         <> 
-           
-            <p> {dosingName}</p>
-            {/*
             <img className="treatment-info" src={aaa} alt={treatment}></img>
-            <img className="treatment-info" src={require(str)} alt={treatment}></img>  */}
-           
-            {/*
-
-            <img className="treatment-info" src={require(`${dosingName}`)}alt={treatment}></img>
-
-            */}
-            <img className="treatment-info" src={require(`${test}`)}alt={treatment}></img>
-            <ReactImageFallback
-                    src={require(`${dosingName}`)}
-                    fallbackImage={aaa}
-                    initialImage="loader.gif"
-                    alt="missing img"
-                    className="treatment-info" />
         </>
     );
 }
