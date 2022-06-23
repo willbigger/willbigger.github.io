fff5b2dd8c81bd9430e13ca4b62f7e116b6a7450
calvinkuo <14984741+calvinkuo@users.noreply.github.com>
2022-06-23 14:52:48 -0400

    Add new baby image

--- frontend/src/components/FormComponentHeader.js
+++ frontend/src/components/FormComponentHeader.js
@@ -1,16 +1,16 @@
-import baby from "./baby.jpeg";
+import baby from "./baby.jpg";
 
 import './FormComponentHeader.css';
 
 function FormComponentHeader() {
   return (
-    <header>
-      <img className="image" src={baby} alt="" />
-      <h1 className="header">This web-based application is a tool to aid neonatal clinicians in evidenced-based antibiotic stewardship. By entering a few inputs, this web-app will give guidance to: 
-      Stop or use fewer doses and days of antibiotics when cultures are negative 
-      Select specific antibiotics based on patient’s risk factors and clinician’s assessment when performing sepsis evaluations
-      Help guide clinicians to select the narrowest spectrum of antibiotics for treatment when pathogens are isolated
-</h1>
+    <header style={{backgroundImage: `url(${baby})`}}>
+      <h1 className="header">
+        This web-based application is a tool to aid neonatal clinicians in evidenced-based antibiotic stewardship. By entering a few inputs, this web-app will give guidance to:
+        Stop or use fewer doses and days of antibiotics when cultures are negative
+        Select specific antibiotics based on patient’s risk factors and clinician’s assessment when performing sepsis evaluations
+        Help guide clinicians to select the narrowest spectrum of antibiotics for treatment when pathogens are isolated
+      </h1>
     </header>
   )
 }
