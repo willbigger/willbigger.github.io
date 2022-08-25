7a94c7123c0efd8db244737e5c3b4ded24b66763
Morgan Hale <mah7ks@virginia.edu>
2022-08-24 22:44:46 -0400

    Update FormComponentHeader.js
    removed "" in formComponent header text

--- frontend/src/components/FormComponentHeader.js
+++ frontend/src/components/FormComponentHeader.js
@@ -8,12 +8,12 @@ function FormComponentHeader() {
       <div class="header-container">
         <div class="header-img" style={{backgroundImage: `url(${baby})`}}></div>
         <div class="header-text">
-          <h5>“Neonatal Antibiotic Stewardship Clinical Decision Support Tool. </h5>
+          <h5>Neonatal Antibiotic Stewardship Clinical Decision Support Tool. </h5>
           <p>This web-based application is a tool to aid neonatal clinicians in 
             evidenced-based antibiotic stewardship. 
             It will guide clinicians to stop or use the fewest days of antibiotics when cultures 
             are negative and select the narrowest spectrum of antibiotics for treatment when
-             pathogens are isolated.”</p>
+             pathogens are isolated.</p>
           {/*
           <ul>
             <li>Stop or use fewer doses and days of antibiotics when cultures are negative</li>
